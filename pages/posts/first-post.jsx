1d70069c442614ad14c5409b5f779ad945cc83f2
Luis <vsjoseluisvs@gmail.com>
2022-11-14 20:03:59 -0600

    Global styles added

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
 
+import Layout from '../../components/layout';
+
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
-      
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
@@ -20,8 +22,9 @@ const FirstPost = () => {
         height={144} // Desired size with correct aspect ratio
         width={144} // Desired size with correct aspect ratio
         alt="me xd"
+        priority={ true }
       />
-    </>
+    </Layout>
   )
 }
 
